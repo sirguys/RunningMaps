126b1911505b26e4db2627653fb1afcac19301ec
sarawut kodkeaw <sirguys.apple@gmail.com>
2017-06-24 17:17:00 +0700

    create components and interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ActivityListComponent } from './activity-list/activity-list.component';
+import { MapComponent } from './map/map.component';
+
+import { ActivityService } from './services/activity.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActivityListComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ActivityService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

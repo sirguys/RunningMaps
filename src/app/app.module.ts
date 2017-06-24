516602ca0b2861a47a1640f053d88ab33a5e9890
sarawut kodkeaw <sirguys.apple@gmail.com>
2017-06-24 22:27:49 +0700

    just add api access token from mapbox into environment.MAPBOX_API_KEY

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { ActivityListComponent } from './activity-list/activity-list.component';
 import { MapComponent } from './map/map.component';
 
 import { ActivityService } from './services/activity.service';
+import { MapService } from './services/map.service';
+import { appRoutes } from '../routes';
 
 @NgModule({
   declarations: [
@@ -14,9 +19,12 @@ import { ActivityService } from './services/activity.service';
     MapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [ActivityService],
+  providers: [ActivityService, MapService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
